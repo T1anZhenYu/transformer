c1f6226ab2066fa112dc4c97bb1a5ac937ac97bd
tian <t18215595124@gmail.com>
2019-05-21 22:00:26 +0800

    this is the model of len_pre_transformer

--- 2_len_pre_transformer.py
+++ 2_len_pre_transformer.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree. An additional grant of patent rights
 # can be found in the PATENTS file in the same directory.
-
+# only predict tgt sentence length
 import math
 
 import torch
